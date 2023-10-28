34e915fcf6ac758cc0cb9fbc403627d850eb0978
sanzharMinbaev <sanman233@gmail.com>
2023-10-28 22:55:36 +0600

    Added method, that returns matrix with ships

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
-import java.util.Set;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Random;
 
 // NOTE
 // Matrix (user 7x7;)Ships(1x3; 1x2 2; 1x1 4;)
 // 
 // 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Random rand = new Random();
+public class Main 
+{
+    public static void main(String[] args) throws Exception 
+    {
+
         Scanner sc = new Scanner(System.in);
-        // filling field
+        int s = 9;
+        String[][] hidenField = fieldConstructor();
+
+        // output field 7x7
+        for (int i = 1; i < s - 1; i++) {
+            for (int j = 1; j < s - 1; j++) {
+                System.out.print(hidenField[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    // 
+    // Creating matrix with ships
+    // 
+    public static String[][] fieldConstructor() 
+    {
+        Random rand = new Random();
         int s = 9;
         String[][] field = new String[s][s];
         for (int i = 0; i < s; i++) {
@@ -20,57 +35,100 @@ public class Main {
                 field[i][j] = "0";
             }
         }
-        // x3 ship
-        int r1 = 0;
-        int c1 = 0;
-        int randDir = rand.nextInt(1, 3);
-        System.out.println(randDir); // log
+        // 1x3 ship
+        int row = 0;
+        int col = 0;
+        int randDir = 0;
+        randDir = rand.nextInt(1, 3);
         if (randDir == 1)// Y
         {
-            r1 = rand.nextInt(1, 6);
-            c1 = rand.nextInt(1, 6);
+            row = rand.nextInt(1, 6);
+            col = rand.nextInt(1, 6);
             for (int i = 0; i < 3; i++) {
-                field[r1 + i][c1] = "X";
-                field[r1 + i][c1 - 1] = "^";
-                field[r1 + i][c1 + 1] = "^";
+                field[row + i][col] = "X";
+                field[row + i][col - 1] = "^";
+                field[row + i][col + 1] = "^";
             }
-            field[r1 - 1][c1] = "^";
-            field[r1 - 1][c1 + 1] = "^";
-            field[r1 - 1][c1 - 1] = "^";
-            field[r1 + 3][c1] = "^";
-            field[r1 + 3][c1 - 1] = "^";
-            field[r1 + 3][c1 + 1] = "^";
-        } 
-        else if (randDir == 2) {// X
-            r1 = rand.nextInt(1,6);
-            c1 = rand.nextInt(1,6);
+            field[row - 1][col] = "^";
+            field[row - 1][col + 1] = "^";
+            field[row - 1][col - 1] = "^";
+            field[row + 3][col] = "^";
+            field[row + 3][col - 1] = "^";
+            field[row + 3][col + 1] = "^";
+        } else if (randDir == 2) {// X
+            row = rand.nextInt(1, 6);
+            col = rand.nextInt(1, 6);
             for (int i = 0; i < 3; i++) {
-                field[r1][c1 + i] = "X";
+                field[row][col + i] = "X";
                 //
-                field[r1 - 1][c1 + i] = "^";
-                field[r1 + 1][c1 + i] = "^";
+                field[row - 1][col + i] = "^";
+                field[row + 1][col + i] = "^";
             }
-            field[r1][c1 - 1] = "^";
-            field[r1 - 1][c1 - 1] = "^";
-            field[r1 + 1][c1 - 1] = "^";
-            field[r1][c1 + 3] = "^";
-            field[r1 - 1][c1 + 3] = "^";
-            field[r1 + 1][c1 + 3] = "^";
+            field[row][col - 1] = "^";
+            field[row - 1][col - 1] = "^";
+            field[row + 1][col - 1] = "^";
+            field[row][col + 3] = "^";
+            field[row - 1][col + 3] = "^";
+            field[row + 1][col + 3] = "^";
         }
-        // output field 9x9
-        // for (int i = 0; i < s; i++) {
-        //     for (int j = 0; j < s; j++) {
-        //         System.out.print(field[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-        // output field 7x7
-        for (int i = 1; i < s - 1; i++) {
-            for (int j = 1; j < s -1; j++) {
-                System.out.print(field[i][j] + " ");
+        // 1x2 ships
+        row = 0;
+        col = 0;
+        int ships = 0;
+        while (ships != 2) {
+            randDir = rand.nextInt(1, 3);
+            row = rand.nextInt(1, 7);
+            col = rand.nextInt(1, 7);
+            if (randDir == 1 && field[row][col].equals("0") && field[row + 1][col].equals("0")) // Y
+            {
+                for (int i = 0; i < 2; i++) {
+                    field[row + i][col] = "X";
+                    field[row + i][col - 1] = "^";
+                    field[row + i][col + 1] = "^";
+                }
+                field[row - 1][col] = "^";
+                field[row - 1][col + 1] = "^";
+                field[row - 1][col - 1] = "^";
+                field[row + 2][col] = "^";
+                field[row + 2][col - 1] = "^";
+                field[row + 2][col + 1] = "^";
+                ships += 1;
+            } else if (randDir == 2 && field[row][col].equals("0") && field[row][col + 1].equals("0")) {
+                for (int i = 0; i < 2; i++) {
+                    field[row][col + i] = "X";
+                    //
+                    field[row - 1][col + i] = "^";
+                    field[row + 1][col + i] = "^";
+                }
+                field[row][col - 1] = "^";
+                field[row - 1][col - 1] = "^";
+                field[row + 1][col - 1] = "^";
+                field[row][col + 2] = "^";
+                field[row - 1][col + 2] = "^";
+                field[row + 1][col + 2] = "^";
+                ships += 1;
+            } else {
+                continue;
             }
-            System.out.println();
         }
-
+        // 1x1 ships
+        ships = 0;
+        while (ships != 4) {
+            row = rand.nextInt(1, 8);
+            col = rand.nextInt(1, 8);
+            if (field[row][col].equals("0") == true) {
+                field[row][col] = "X";
+                field[row][col - 1] = "^";
+                field[row][col + 1] = "^";
+                field[row - 1][col] = "^";
+                field[row + 1][col] = "^";
+                field[row - 1][col + 1] = "^";
+                field[row - 1][col - 1] = "^";
+                field[row + 1][col - 1] = "^";
+                field[row + 1][col + 1] = "^";
+                ships++;
+            }
+        }
+        return field;
     }
 }
